54a5c4f59cf6390c0b08312ae0bfe04d8a26a95b
LeszczynskiKarol <leszczu123@wp.pl>
2026-02-11 13:01:17 +0100

    poprawka seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://by-interior.pl",
+  site: "https://www.by-interior.pl",
   integrations: [tailwind()],
 });
